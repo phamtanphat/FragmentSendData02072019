b97e1af353fe4116eb2881b9cbdc4366c587044b
phatdroid94 <phatdroid94@gmail.com>
2019-10-10 18:32:14 +0700

    feature/send data activity to fragment rating

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/fragmentsenddata/MainActivity.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/fragmentsenddata/MainActivity.java
@@ -1,14 +1,50 @@
 package phamtanphat.ptp.khoaphamtraining.fragmentsenddata;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
+    RadioGroup radioGroup;
+    FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        radioGroup = findViewById(R.id.radioGroup);
+        fragmentManager = getSupportFragmentManager();
+
+        // 1 : Fragment chua ton tai trong activity
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//                switch (i){
+//                    case R.id.radioBad :
+//                        break;
+//                    case R.id.radioLike:
+//                        break;
+//                    case R.id.radioEnjoy:
+//                        break;
+//                }
+                RatingFragment ratingFragment = new RatingFragment();
+                Bundle bundle = new Bundle();
+                bundle.putInt("numstar", 1);
+                ratingFragment.setArguments(bundle);
+                fragmentTransaction.add(R.id.linearContainer,ratingFragment);
+                fragmentTransaction.commit();
+            }
+        });
+
+
+
+
+        // 2 : Fragment da nam trong activity
     }
 }
